1210b701a523a6f4110f228bb92810b5dd0de42b
halldorrt <halldorr@gmail.com>
2017-05-28 23:32:45 +0000

    Change AppleDelegate to reenable debugger

--- ios/takemeout_ios/AppDelegate.m
+++ ios/takemeout_ios/AppDelegate.m
@@ -18,7 +18,10 @@
 {
   NSURL *jsCodeLocation;
 
- jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+// For simulator deployment
+// jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
   
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"takemeout_ios"
